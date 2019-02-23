8b3e7ac31953e5887f9845302c44c3677d787ee5
alex <alex.konofagos@dtmh.gr>
2019-02-23 10:09:50 +0200

    Initial commit

---
+++ Assets/ColorAverage.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorAverage : MonoBehaviour
+{
+	public Texture2D tex;
+	public Material target, target2;
+    // Start is called before the first frame update
+    void Start()
+    {
+		Color[] cols = tex.GetPixels();
+		Color average = cols[0];
+		//left to right, bottom to top
+		for(int w = 1; w < cols.Length; w++) {
+				average = (average + cols[w]) / 2;
+		}
+
+
+		for(int w = 1; w < cols.Length; w++) {
+			cols[w] -= (average * average);
+		}
+
+		Texture2D nTex = new Texture2D(tex.width, tex.height, tex.format, false);
+
+		nTex.SetPixels(cols);
+		nTex.Apply();
+		target.SetTexture("_MainTex", nTex);
+
+		Texture2D nTex2 = new Texture2D(tex.width, tex.height, tex.format, false);
+		average = cols[0];
+		for(int w = 1; w < cols.Length; w++) {
+			average = (average + cols[w]) / 2;
+		}
+
+		for(int w = 1; w < cols.Length; w++) {
+			cols[w] = (cols[w] * average);
+		}
+
+		Debug.Log(average);
+		nTex2.SetPixels(cols);
+		nTex2.Apply();
+		target2.SetTexture("_MainTex", nTex2);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
